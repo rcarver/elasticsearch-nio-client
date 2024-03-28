9ad32f3032905462f607267a5546126179b0fda2
Ryan Carver <ryan@ryancarver.com>
2024-03-27 20:06:13 -0700

    add `score` to the hits document

--- Sources/ElasticsearchNIOClient/Models/ESSingleDocumentResponse.swift
+++ Sources/ElasticsearchNIOClient/Models/ESSingleDocumentResponse.swift
@@ -5,11 +5,13 @@ public struct ESGetSingleDocumentResponse<Document: Decodable>: Decodable {
     public let index: String
     public let version: Int?
     public let source: Document
-    
+    public let score: Double
+
     enum CodingKeys: String, CodingKey {
         case id = "_id"
         case index = "_index"
         case version = "_version"
         case source = "_source"
+        case score = "_score"
     }
 }
